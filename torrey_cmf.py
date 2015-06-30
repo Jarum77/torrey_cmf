5e583717665d1d3d1ee494d31d9f05f1e1f14213
Paul Torrey <ptorrey@mit.edu>
2015-06-30 12:08:47 -0400

    updates to fitting form and coefficients.

--- torrey_cmf.py
+++ torrey_cmf.py
@@ -20,23 +20,22 @@ from scipy.optimize import newton
 class number_density:
     def __init__(self):
         self._cmf_vars = np.array(
-        [   [-4.02977287e+00,   1.76267393e-01,  -1.44941371e-01],
-            [-1.05309169e+00,   1.17135834e-01,  -5.40660745e-02],
-            [-8.58905816e-02,   2.52993131e-02,  -6.90611852e-03],
-            [1.20106042e+01,   -2.34838939e-01,   2.52957319e-02]])
+	[ [-2.89381061e+00,   8.21989048e-02,  -1.23157487e-01],  
+	  [-6.25597878e-01,   8.62157470e-02,  -4.90327135e-02],  
+	  [-3.88949868e-02,   2.54185320e-02,  -7.12953126e-03],   
+	  [ 1.15238521e+01,  -1.87102380e-01,   2.10223127e-02] ] )
 
         self._dmf_vars = np.array(
-	[ [-3.78562499e+00,   1.86168941e-01,  -1.38846077e-01],  
-	  [-9.65414752e-01,   1.32918334e-01,  -5.68567233e-02],  
-	  [-8.77794305e-02,   3.21826643e-02,  -8.01041519e-03],   
-	  [ 1.20829904e+01,  -2.46826596e-01,   2.41133674e-02]] )
-
+ [   [     -4.58622836,        0.32510541,      -0.00729362],
+     [     -0.85569723,       -0.11641107,       0.00950263],
+     [      0.00190046,       -0.01466030,       0.00173920],
+     [     14.14366307,       -0.60796535,       0.02010243]  ] )
 
         self._cvdf_vars = np.array(
-        [   [-2.34444132,  0.24249917, -0.10259731],
-            [-1.47270662,  0.54513093, -0.22659028],
-            [0.44307961,  1.21992189, -0.28089004],
-            [2.3837376,  -0.04416525,  0.00729029]])
+ [   [      7.39149763,        5.72940031,      -1.12055245],
+     [     -6.86339338,       -5.27327109,       1.10411386],
+     [      2.85208259,        1.25569600,      -0.28663846],
+     [      0.06703215,       -0.04868317,       0.00764841]  ] )
 
         self._cdmf_vars = np.array(
         [   [-5.08189685e+00,   1.55357858e-01,  -2.18816909e-02],
@@ -45,56 +44,53 @@ class number_density:
             [1.41924033e+01,  -5.14655812e-01,   2.22309520e-02]])
             
         self._vars_nc = np.array(
-         [  [ -3.99510528e+00,   2.04772606e-01,  -1.43642170e-01],
-            [-1.02669347e+00,    2.55361970e-02,  -6.16581730e-02],
-            [-8.18861264e-02,   1.86254043e-03,   -8.24157788e-03],
-            [1.20001548e+01,   3.51709332e-03,   6.84297177e-02]])
-
-
-
+ [   [     -2.64396065,       -0.29957949,      -0.03786140],
+     [     -0.52684351,       -0.13813571,      -0.03224572],
+     [     -0.02648208,       -0.01600591,      -0.00564523],
+     [     11.33927770,        0.39102542,      -0.01573175]  ])
 
         self._vars_nc_rev3 = np.array(
-        [ [ -4.40077515e+00,  -6.62145255e-01,   1.91921627e-01],
-          [-8.82295647e-01,   -2.33066595e-01,   4.70923783e-02],
-          [-3.97308012e-02,  -2.20131731e-02,    4.09743368e-03],
-           [1.15256908e+01,   3.78107550e-01,    -1.37326740e-01]])
+ [   [     -4.35301221,       -0.43893418,       0.22963251],
+     [     -0.79280392,       -0.19135083,       0.06622329],
+     [     -0.02908882,       -0.02000640,       0.00739485],
+     [     11.80107352,        0.19790054,      -0.14974796]  ])
 
         self._vars_nc_rev2 = np.array(
-        [ [ -4.19858527e+00,  -4.49363598e-01,   2.29778257e-01],
-          [ -9.23353873e-01,   -1.83275908e-01,   6.85051354e-02],
-          [ -5.38416102e-02,  -1.72440142e-02,    6.82696341e-03],
-          [ 1.16729458e+01,   2.72053797e-01,  -1.56547865e-01]])
+ [   [     -3.99168548,        0.34870581,       0.05096602],
+     [     -0.79382527,        0.01932935,       0.03692425],
+     [     -0.03829736,       -0.00023232,       0.00694828],
+     [     11.82842896,       -0.28090410,      -0.03059818]  ])
 
         self._vars_nc_rev_z1 = np.array(
-        [ [-4.17225261e+00,   1.44894696e-01,   2.88689730e-02],
-          [-1.02895757e+00,   3.94126650e-02,  -1.95460326e-03],
-          [-7.53027534e-02,   9.59023782e-03,  -2.04448868e-03],
-          [1.18825325e+01,  -2.53438989e-02,  -5.30882019e-02]])
+ [   [     -3.64009890,        1.03601036,      -0.31161953],
+     [     -0.79721518,        0.29299124,      -0.07746618],
+     [     -0.04838342,        0.03363121,      -0.00486779],
+     [     11.82759108,       -0.66505107,       0.18483682]  ])
 
 
-    def cmf_fit(self, log_mass, redshift, target=0):
+    def cmf_fit(self, log_mass, redshift, target=0, **kwargs):
         """ Evaluate the CMF at a given list of masses at some redshift"""
         if np.max(log_mass) > 20: warn_not_log_arg()
         return self._cmf_fit_func(log_mass, self._cmf_vars, redshift, target=target)
 
-    def cvdf_fit(self, log_vd, redshift, target=0):
+    def cvdf_fit(self, log_vd, redshift, target=0, **kwargs):
         """ Evaluate the CVDF at a given list of vd's at some redshift"""
         if np.max(log_vd) > 10: warn_not_log_arg()
         return self._cmf_fit_func(log_vd, self._cvdf_vars, redshift, target=target)
 
-    def cdmf_fit(self, log_mass, redshift, target=0):
+    def cdmf_fit(self, log_mass, redshift, target=0, **kwargs):
         """ Evaluate the dm CMF at a given list of masses at some redshift"""
         if np.max(log_mass) > 20: warn_not_log_arg()
         return self._cmf_fit_func(log_mass, self._cdmf_vars, redshift, target=target)
 
-    def gsmf_fit(self, log_mass, redshift, target=0):
+    def gsmf_fit(self, log_mass, redshift, target=0, **kwargs):
         """ Evaluate the GSMF at a given list of masses at some redshift"""
         if np.max(log_mass) > 20: warn_not_log_arg()
         return self._cmf_fit_func(log_mass, self._dmf_vars, redshift, target=target)
 
-    def nc_cmf_fit(self, log_mass, redshift, target=0, z_init=0):
-        """ If you feed this a **z_init*** stellar mass, it will return the
-            effective number density of that galaxy population at some other redshift """
+    def nc_cmf_fit(self, log_mass, redshift, target=0, z_init=0, **kwargs):
+        """ If you feed this a **z_init*** stellar mass (log_mass), it will return the
+            median number density of that galaxy population at some other redshift (redshift) """
         if np.max(log_mass) > 20:  warn_not_log_arg()
         if z_init == 0: this_vars = self._vars_nc
         if z_init == 1: this_vars = self._vars_nc_rev1
@@ -126,7 +122,6 @@ class number_density:
         arr = np.array([arr]).flatten()     # input masses or vds at z1
         res = np.zeros_like(arr)            # output masses or vds at z2
         
-
         if field == 'mass':
             from_dens = self.mass_from_density
             if nc:  arr = arr
@@ -142,27 +137,27 @@ class number_density:
 
         if nc:
             to_dens = self.nc_cmf_fit
-            z1 = z2
 
         for i, elem in enumerate(arr):
-            proj_dens = to_dens(elem, z1)
-            if proj_dens < np.log10(3e-5):  # you're below the fit limits
+	    if nc:  proj_dens = to_dens(elem, z2, z_init=z1)
+	    else:   proj_dens = to_dens(elem, z1)
+            if proj_dens < np.log10(1e-5):  # you're below the fit limits
+	 	print " "
                 print " PROJECTING GROWTH BELOW FIT LIMITS! "
-                proj_dens = np.log10(3e-5)
+                proj_dens = np.log10(1e-5)
             res[i] = from_dens(proj_dens, z2)
-            if res.shape[0] == 1:
+
+        if res.shape[0] == 1:
                 return res[0]
-            else:
-                return res
+        else:
+           return res
 
     def _cmf_fit_func(self, this_val, this_vars, redshift, target=0):
         """ Evaluate Equations 1 & 2-5 from Torrey+2015 """
         coeffs = [this_vars[i][0] + this_vars[i][1] * redshift +
               this_vars[i][2] * redshift**2 for i in range(4)]
         mstar = this_val - coeffs[3]
-        return coeffs[0] + coeffs[1]*mstar + coeffs[2]*mstar**2 - 10.0**mstar - target
-
-
+        return coeffs[0] + coeffs[1]*mstar + coeffs[2]*mstar**2 - np.exp(mstar) - target
 
 
 def warn_not_log_arg():
